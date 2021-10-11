570e5bf99fab390d5e07a0ea13b11951ababfda3
dev1 <gauri.g@girmesofttech.com>
2021-10-11 14:04:50 +0530

    home page added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import HelloWorld from "./components/HelloWorld";
+import RenderForm from "./components/RenderForm";
+
+const App = () => {
+  return (
+    <div>
+      <center>
+      <HelloWorld />
+      <RenderForm />
+      </center>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Call from "../assets/images/call.png";
+import Arrow from "../assets/images/arrow.svg";
+import Mobile from "../assets/images/mobile.svg";
+
+const HelloWorld = () => {
+  return (
+    <>
+    <div>
+ 
+      <h1>Hello World</h1>
+    </div>
+    {/* <img src={Call} alt="BigCo Inc. logo"/>
+    <img src={Arrow} alt="No arrow "/>
+    <img src={Mobile} alt="No arrow "/> */}
+    </>
+  );
+};
+
+export default HelloWorld;
